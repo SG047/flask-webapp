63e047c93a4b3589fabf3b3199b9d7c0e68641e3
Sindhu <61133818+SG047@users.noreply.github.com>
2021-12-08 11:55:01 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,15 +6,15 @@ from flask_sqlalchemy import SQLAlchemy
 app=Flask(__name__) #name of the module or package
 
 #app.config['SQLALCHEMY_DATABASE_URI'] ='postgresql+psycopg2://postgres:Monday09@localhost/quotes'
-app.config['SQLALCHEMY_DATABASE_URI'] ='postgres://oycordaovdzkrz:a384299ee5c5aa0469f8b5b3d0697a6c553296097ff600530b9f252ea3571d6b@ec2-3-89-214-80.compute-1.amazonaws.com:5432/da5055prgs2k98'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
+#app.config['SQLALCHEMY_DATABASE_URI'] ='postgres://oycordaovdzkrz:a384299ee5c5aa0469f8b5b3d0697a6c553296097ff600530b9f252ea3571d6b@ec2-3-89-214-80.compute-1.amazonaws.com:5432/da5055prgs2k98'
+#app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
 
-db = SQLAlchemy(app)
+#db = SQLAlchemy(app)
 
-class Favquotes(db.Model):
-	id = db.Column(db.Integer,primary_key=True)
-	author = db.Column(db.String(30))
-	quote = db.Column(db.String(2000))
+#class Favquotes(db.Model):
+	#id = db.Column(db.Integer,primary_key=True)
+	#author = db.Column(db.String(30))
+	#quote = db.Column(db.String(2000))
 
 
 @app.route("/")
